efc0d8003396346d3dc939d0b2e1d66595e25795
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-03-06 20:45:56 +0100

    Weapon system 01 and debugging tools 01

---
+++ Shotgun Goblin/Assets/Project/Scripts/Ansgars Testing Scripts/PlaceholderGunScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceholderGunScript : MonoBehaviour, IHeldItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void DoAcction()
+    {
+        Debug.Log("Pow");
+    }
+
+
+}
