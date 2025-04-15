609817d84b6405d4573e9206f18292637e83d1b6
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-04-15 18:40:08 +0200

    FPS counter 2

--- Shotgun Goblin/Assets/Project/Scripts/Ansgars Testing Scripts/FPS_Counter.cs
+++ Shotgun Goblin/Assets/Project/Scripts/Ansgars Testing Scripts/FPS_Counter.cs
@@ -5,25 +5,82 @@ using UnityEngine;
 
 public class FPS_Counter : MonoBehaviour
 {
-    public float FPS;
-    protected float UnRoundedFPS;
 
+    [Header("FPS values")]
+
+    public float FPS_Avrage;
+    
+    public float FPS;
+    
     public float True_FPS;
 
-    [SerializeField] int decimals;
-    [SerializeField] float lerpValue = 0.5f;
+    protected float UnRounded_FPS_Avrage;
+
+
+    [Header("FPS_Avrage Variables")]
+    [SerializeField] int FrameSampleInterval = 50;
+    [SerializeField] int FrameCounter;
+    protected float FPS_Sum;
+
+
+    [Header("FPS Variables")]
+    [SerializeField] int Decimals;
+    [SerializeField] float Smootheness = 0.3f;
+
+    protected float UnRounded_FPS;
+
+    
+
+
+    
+
+    
+
+    
+    
     
 
    
     void Update()
     {
+        float fps = SampleFPS();
+
+        AvrageFPS(fps);
+
+    }
+
+    protected void AvrageFPS(float fps)
+    {
+        FPS_Sum += fps;
+
+        FrameCounter = Time.frameCount % FrameSampleInterval;
+
+        if(FrameCounter == 0)
+        {
+            UnRounded_FPS_Avrage = FPS_Sum / (float)FrameSampleInterval;
+            FPS_Sum = 0;
+        }
+
+        FPS_Avrage = Round(UnRounded_FPS_Avrage, Decimals);
+
+    }
+
+    protected float SampleFPS()
+    {
         True_FPS = 1f / Time.deltaTime;
 
-        UnRoundedFPS = UnRoundedFPS * (1-lerpValue) + True_FPS * lerpValue;
+        UnRounded_FPS = UnRounded_FPS * (1 - Smootheness) + True_FPS * Smootheness;
 
+        FPS = Round(UnRounded_FPS, Decimals);
+
+        return FPS;
+    }
+
+    protected float Round(float value, float decimals)
+    {
         float decimalPower = math.pow(10f, decimals);
 
-        FPS = math.round(UnRoundedFPS * decimalPower) / decimalPower;
-        
+        return math.round(value * decimalPower) / decimalPower;
+
     }
 }
