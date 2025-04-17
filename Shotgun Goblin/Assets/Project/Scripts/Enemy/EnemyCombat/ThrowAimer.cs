bee0bc151f020af61014ed4ec18afc7f400655d3
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-04-17 17:44:25 +0200

    Throw Aim 2

--- Shotgun Goblin/Assets/Project/Scripts/Enemy/EnemyCombat/ThrowAimer.cs
+++ Shotgun Goblin/Assets/Project/Scripts/Enemy/EnemyCombat/ThrowAimer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ThrowAimer : MonoBehaviour
 {
+    [SerializeField] GameObject RotationObject;
     protected enum TypeOfThrow
     {
         High,
@@ -11,15 +12,36 @@ public class ThrowAimer : MonoBehaviour
     }
 
     [SerializeField] TypeOfThrow typeOfThrow;
+    [SerializeField] float MaxVerticalAngle = 90;
+    [SerializeField] float MinVerticalAngle = -90;
+
+    [SerializeField] float MaxHorizontalAngle = 90;
+    [SerializeField] float MinHorizontalAngle = -90;
+
+    protected Vector3 upDirection => Physics.gravity.normalized;
+
     
 
-    public void Aim()
+    public void Aim(Vector3 Pos, float StartVelocity)
     {
         
         
     }
 
+    protected Vector3 GetDirection(Vector3 ThisPos, Vector3 TargetPos)
+    {
+        return TargetPos - ThisPos;
+    }
+    
 
+    protected Vector2 ToComposantsInDirection(Vector3 Direction, Vector3 up)
+    {
+
+        Vector3 groundDistance = Vector3.ProjectOnPlane(Direction, up);
+        Vector3 hight = Vector3.Project(Direction,up);
+
+        return new Vector2(0, 0);
+    }
     
 
 }
