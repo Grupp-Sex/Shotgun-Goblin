4e4ef46f55ae4bc02ed1acdc030134f03fef1212
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-05-11 21:43:28 +0200

    new shotgun particles 1

--- Shotgun Goblin/Assets/Project/Scripts/TelekenesisScripts/RemoveNavObsticleOnTelekenesis.cs
+++ Shotgun Goblin/Assets/Project/Scripts/TelekenesisScripts/RemoveNavObsticleOnTelekenesis.cs
@@ -3,24 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class RemoveNavObsticleOnTelekenesis : MonoBehaviour, IOnTelekenesisEnter, IOnTelekenesisLeave
+public class RemoveNavObsticleOnTelekenesis : MonoBehaviour
 {
+    public TelekenesisPhysicsObject TelekenesisPhysicsObject;
     public NavMeshObstacle NavMeshObstacle;
     [SerializeField] bool ReEnable = false;
 
-    private void Start()
+    private void Awake()
     {
         if(NavMeshObstacle == null)
         {
             NavMeshObstacle = GetComponent<NavMeshObstacle>();
         }
+
+        if(TelekenesisPhysicsObject == null)
+        {
+            TelekenesisPhysicsObject = GetComponent<TelekenesisPhysicsObject>();
+        }
+
+        
     }
-    public void OnTelekenesisEnter()
+
+    private void OnEnable()
+    {
+        if (TelekenesisPhysicsObject != null)
+        {
+            TelekenesisPhysicsObject.Event_OnTelekenesisEnter.Subscribe(Event_TelekenesisEnter);
+            TelekenesisPhysicsObject.Event_OnTelekenesisEnter.Subscribe(Event_TelekenesisLeave);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (TelekenesisPhysicsObject != null)
+        {
+            TelekenesisPhysicsObject.Event_OnTelekenesisEnter.UnSubscribe(Event_TelekenesisEnter);
+            TelekenesisPhysicsObject.Event_OnTelekenesisEnter.UnSubscribe(Event_TelekenesisLeave);
+        }
+    }
+
+    public void Event_TelekenesisEnter(object sender, TelekenesisPhysicsObject args)
     {
         NavMeshObstacle.enabled = false;
     }
 
-    public void OnTelekenesisLeave()
+    public void Event_TelekenesisLeave(object sender, TelekenesisPhysicsObject args)
     {
         if (ReEnable)
         {
