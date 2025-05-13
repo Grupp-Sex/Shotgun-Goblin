2458b0e412e742aa34cdcdd693770531d1841878
Yelluss <mikael.paluch@gmail.com>
2025-05-13 23:19:30 +0200

    dash in progress

--- Shotgun Goblin/Assets/Project/Scripts/Player/Dash/PlayerDash.cs
+++ Shotgun Goblin/Assets/Project/Scripts/Player/Dash/PlayerDash.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerDash : MonoBehaviour
 {
+
+    [Header("References")]
+    public PlayerCam cam;
+    private PlayerMovement movement;
+    
+
+    //Vet inte för tillfället hur Ansgars kod fungerar så vi kan byta till inheritance
+    private Rigidbody rb;
+
+    [Header("Dashing")]
+    [SerializeField] private float dashForce;
+    [SerializeField] private float dashTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        movement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    private void OnDashStart()
+    {
+
+    }
+
+    private void dash()
+    {
+        Vector3 forceToApply = 
     }
 }
