37c7d8ce1042c3f5e3d0391f2a02575a5860f9ee
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-05-20 15:54:33 +0200

    kill talley and player hurd effect

---
+++ Shotgun Goblin/Assets/Project/Scripts/DamageHealth/ActivatedScripts/DamageHudOverlay.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DamageHudOverlay : MonoBehaviour, IDamageActivated
+{
+    protected HealthManager healthManager;
+    [SerializeField] private AnimationCurve curve;
+    [SerializeField] private float lerpValue;
+    public Volume urpVolume;
+
+    protected float targetValue;
+    protected float currentValue;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        healthManager = GetComponent<HealthManager>();
+    }
+
+    public void OnDamage(DamageInfo damageInfo)
+    {
+        CalculateHealthLerp();
+    }
+
+    protected void CalculateHealthLerp()
+    {
+        float lerpValue = healthManager.Health / healthManager.MaxHealth;
+
+        lerpValue = Mathf.Clamp01(1 - lerpValue);
+
+        float curveValue = curve.Evaluate(lerpValue);
+
+        targetValue = Mathf.Clamp01(curveValue);
+    }
+
+    protected void Interpolate()
+    {
+        currentValue = currentValue * lerpValue + targetValue * (1-lerpValue);
+    }
+
+    protected void UpdateVolume()
+    {
+        if(urpVolume != null)
+        {
+            urpVolume.weight = currentValue;
+        }
+    }
+
+    private void Update()
+    {
+        Interpolate();
+        UpdateVolume();
+    }
+
+
+}
+
+
