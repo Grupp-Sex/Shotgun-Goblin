4b6da98e57ce4b5aab136d50fc03a5728d7a39cf
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-05-25 20:13:30 +0200

    Hole decal

---
+++ Shotgun Goblin/Assets/Project/Scripts/Victory/NextLevelZone.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class NextLevelZone : MonoBehaviour
+{
+    [SerializeField] private float timer;
+    public EventPusher<object> Event_GameEnd = new EventPusher<object>();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartEndGameTimer();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    protected void StartEndGameTimer()
+    {
+        StartCoroutine(EndGameTimer(timer));
+    }
+
+    protected IEnumerator EndGameTimer(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        EndGame();
+    }
+
+    protected void EndGame()
+    {
+        Event_GameEnd.Invoke(this, null);
+    }
+}
