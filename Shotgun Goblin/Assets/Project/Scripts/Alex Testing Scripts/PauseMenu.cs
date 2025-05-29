0e10c9767a171f55431e8463de0abe0c1c5364e0
childpie <alexandra.i.renner@gmail.com>
2025-05-29 14:03:44 +0200

    pause sound
    added beginning of sound script
    added pause menu UI

---
+++ Shotgun Goblin/Assets/Project/Scripts/Alex Testing Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    //This bool will be needed as a reference in other
+    //scripts so that no actions may occur when paused.
+    public static bool isActive;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isActive) 
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isActive = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        isActive = false;
+    }
+}
