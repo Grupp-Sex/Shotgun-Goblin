b8472fd5eb9561033c7b25eac33a62f1ea5d910f
childpie <alexandra.i.renner@gmail.com>
2025-05-30 12:47:26 +0200

    fixed pause menu bug
    and some audio script

--- Shotgun Goblin/Assets/Project/Scripts/Audio/AudioManager.cs
+++ Shotgun Goblin/Assets/Project/Scripts/Audio/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Pool;
+using UnityEngine.UIElements;
 
 
 public class AudioManager : MonoBehaviour
@@ -23,12 +24,12 @@ public class AudioManager : MonoBehaviour
 
     [Header("Mixer & SubGroups")]
     [SerializeField] private AudioMixer masterMixer;
-    [SerializeField] private AudioMixerGroup musicMixGroup, sfxMixGroup, ambientMixGroup, uiMixGroup;
+    [SerializeField] private AudioMixerGroup sfxMixGroup, ambientMixGroup, uiMixGroup;
 
     
+    // sfxPoolSize is just an integer that hold a maximum of Active sounds
     [Header("3D Audio Source Prefab")]
     [SerializeField] private AudioSource audio3DPrefab;
-    // SFXPoolSize is just an integer that hold a maximum of Active sounds
     [SerializeField] private int sfxPoolMaxSize = 20;
 
     private ObjectPool<AudioSource> sourcePool;
@@ -56,7 +57,10 @@ public class AudioManager : MonoBehaviour
             createFunc: (CreateAudioSourcePool),
             actionOnGet: GetPooledAudioSource,
             actionOnRelease: ReleasePooledAudioSource,
-            actionOnDestroy: DestroyPooledAudioSource
+            actionOnDestroy: DestroyPooledAudioSource,
+            collectionCheck: false,
+            defaultCapacity: sfxPoolMaxSize, 
+            maxSize: sfxPoolMaxSize * 2
             );
     }
 
@@ -70,6 +74,47 @@ public class AudioManager : MonoBehaviour
         return src;
     }
 
+
+    public void Play3DSound(AudioClip clip, bool is3D, Vector3 position = default, AudioMixerGroup group = null, float volume = 1f, float pitch = 1f)
+    {
+        //Make sure to not play the sound if there's no audioclip
+        if (clip == null)
+            return;
+
+        var source = sourcePool.Get();
+        source.clip = clip;
+        source.outputAudioMixerGroup = group ?? sfxMixGroup; //Assigns the input group into the source, and if it's null it'll by default go into sfxMixGroup
+        source.volume = volume;
+        source.pitch = pitch;
+        source.spatialBlend = is3D ? 1f : 0f; //1f == 3D sound, 0f == 2D sound
+
+        if (is3D)
+        {
+            source.transform.position = position;
+        }
+        else
+        {
+            source.transform.position = Vector3.zero;
+        }
+
+        source.Play();
+
+        /*To avoid the source from releasing it back to the pool right away, risking it to cut off the audio,
+         we use a coroutine. Coroutines is a feature that lets you pause execution and resume it later. Since
+         Unity's main Update loop doesn't support waiting/sleeping, the best option is to use a coroutine 
+         to delay an action asynchronously.*/
+        StartCoroutine(ReleaseAfterPlay(source));
+    }
+
+    private IEnumerator ReleaseAfterPlay(AudioSource source)
+    {
+        /*The yield here is what gives us the delay, and instead of using WaitForSeconds
+         we wait until the source finish playing. This makes sure that the audioclip in the
+         source gets to finish before it get released. */
+        yield return new WaitWhile(() => source.isPlaying);
+        sourcePool.Release(source);
+    }
+
     private void GetPooledAudioSource(AudioSource src)
     {
         src.gameObject.SetActive(true);
@@ -77,7 +122,7 @@ public class AudioManager : MonoBehaviour
 
     private void ReleasePooledAudioSource(AudioSource src)
     {
-        src.gameObject?.SetActive(false);
+        src.gameObject.SetActive(false);
     }
 
     private void DestroyPooledAudioSource(AudioSource src)
