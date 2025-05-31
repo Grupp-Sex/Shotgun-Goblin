95e1f81da40c423cee639a828c8ceca4d7249e9c
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-05-31 15:26:28 +0200

    leve 2 lava

---
+++ Shotgun Goblin/Assets/Project/Scripts/Zones/KillZone.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] List<string> IgnoreTags;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!IgnoreTags.Contains(other.tag))
+        {
+            KillObject(other.GetComponent<HealthManager>());
+        }
+    }
+
+    protected void KillObject(HealthManager helth)
+    {
+        if (helth != null)
+        {
+            helth.Damage(999999999f, helth.transform.position);
+            helth.Damage(helth.MaxHealth, helth.transform.position);
+        }
+    }
+}
