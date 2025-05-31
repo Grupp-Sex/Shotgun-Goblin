1c84d59fcc303830a4866b9e9ea58e331f3750b8
Moon-of-jupiter <ansgar.brunskog@gmail.com>
2025-05-31 15:32:08 +0200

    minor changes

---
+++ Shotgun Goblin/Assets/Project/Scripts/Alex Testing Scripts/PlayerSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerSettings
+{
+    public static bool dash_dubbleTap = true;
+    public static bool dash_shift = true;
+
+    public static float fov = 60f;
+
+}
